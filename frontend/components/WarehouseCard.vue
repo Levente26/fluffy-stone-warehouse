39a589ba9bfc1e36cba8270de6ed098c24235795
Levente26 <madi.levente26@gmail.com>
2023-05-04 18:23:08 +0200

    feat: skeleton of archive warehouse page

---
+++ frontend/components/WarehouseCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>{{ warehouse.attributes.name }}</h2>
+    <!-- <p>{{ warehouse.attributes.address }}</p> -->
+    <p>{{ warehouse.attributes.status }}</p>
+    <p>{{ warehouse.attributes.maximumCapacity }}</p>
+    <p>{{ warehouse.attributes.currentCapacity }}</p>
+
+    <!-- <div v-if="warehouse.attributes.products.data.length === 0">no products</div>
+    <div v-if="warehouse.attributes.products.data.length > 0">
+      <ProductCard
+        v-for="product in warehouse.attributes.products.data"
+        :product="product"
+      />
+    </div> -->
+  </div>
+</template>
+
+<script setup>
+const { warehouse } = defineProps(["warehouse"]);
+</script>
