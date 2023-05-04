bede8853e5be6660c2620b8c636f5e42f3dc7ad6
Levente26 <madi.levente26@gmail.com>
2023-05-04 15:11:23 +0200

    feat: create click outside composable

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
+  <div ref="componentRef" class="general-style">
     <p class="text-3xl font-bold underline">{{ $t("welcome") }}</p>
   </div>
 </template>
 
 <script setup>
+import useDetectOutsideClick from "../composables/useDetectClickOutside";
 const { find } = useStrapi();
 
 const { data, pending, refresh, error } = await useAsyncData("warehouse", () =>
   find("warehouses")
 );
+
+const componentRef = ref();
+
+useDetectOutsideClick(componentRef, () => {
+  console.log("hello");
+});
 </script>
 
 <style lang="scss" scoped>
