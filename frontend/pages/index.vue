7ccbac2254a7b7370ab70d6ef9819dfdb3120b5a
Levente26 <madi.levente26@gmail.com>
2023-05-04 14:40:36 +0200

    feat: add sass to nuxt

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <p class="text-3xl font-bold underline">{{ $t("welcome") }}</p>
+  <div>
+    <p class="text-3xl font-bold underline">{{ $t("welcome") }}</p>
+  </div>
 </template>
 
 <script setup>
@@ -9,3 +11,11 @@ const { data, pending, refresh, error } = await useAsyncData("warehouse", () =>
   find("warehouses")
 );
 </script>
+
+<style lang="scss" scoped>
+div {
+  p {
+    @apply text-3xl text-blue-500 font-bold underline;
+  }
+}
+</style>
