9f4003bdc8b3245dceb95bf3d2dcf8f0de6ad596
Levente26 <madi.levente26@gmail.com>
2023-05-04 19:16:29 +0200

    feat: single warehouse page

--- frontend/components/WarehouseCard.vue
+++ frontend/components/WarehouseCard.vue
@@ -1,21 +1,19 @@
 <template>
-  <div>
+  <div @click="goToSingleWarehouse">
     <h2>{{ warehouse.attributes.name }}</h2>
-    <!-- <p>{{ warehouse.attributes.address }}</p> -->
     <p>{{ warehouse.attributes.status }}</p>
     <p>{{ warehouse.attributes.maximumCapacity }}</p>
     <p>{{ warehouse.attributes.currentCapacity }}</p>
-
-    <!-- <div v-if="warehouse.attributes.products.data.length === 0">no products</div>
-    <div v-if="warehouse.attributes.products.data.length > 0">
-      <ProductCard
-        v-for="product in warehouse.attributes.products.data"
-        :product="product"
-      />
-    </div> -->
   </div>
 </template>
 
 <script setup>
 const { warehouse } = defineProps(["warehouse"]);
+const router = useRouter();
+const localePath = useLocalePath();
+const { locale } = useI18n();
+
+const goToSingleWarehouse = () => {
+  router.push(localePath(`/warehouse/${warehouse.id}`, locale.value));
+};
 </script>
---
+++ frontend/pages/warehouse/[id].vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>Warehouse {{ data.data.attributes.name }} by Fluffy Stone</h1>
+  <h2>{{ data.data.attributes.name }}</h2>
+  <p>{{ data.data.attributes.address }}</p>
+  <p>{{ data.data.attributes.status }}</p>
+  <p>{{ data.data.attributes.maximumCapacity }}</p>
+  <p>{{ data.data.attributes.currentCapacity }}</p>
+
+  <div v-if="data.data.attributes.products.data.length === 0">no products</div>
+  <div v-if="data.data.attributes.products.data.length > 0">
+    <ProductCard
+      v-for="product in data.data.attributes.products.data"
+      :product="product"
+    />
+  </div>
+</template>
+
+<script setup>
+const { findOne } = useStrapi();
+const { locale } = useI18n();
+const localePath = useLocalePath();
+const route = useRoute();
+const router = useRouter();
+
+const { data, pending, refresh, error } = await useAsyncData(
+  "singleWarehouse",
+  () =>
+    findOne("warehouses", route.params.id, {
+      populate: ["products.image", "localizations"],
+      _locale: locale.value,
+    }),
+  {
+    watch: route,
+  }
+);
+
+watch(locale, () => {
+  router.push(
+    localePath(
+      `/warehouse/${data.value.data.attributes.localizations.data[0].id}`,
+      locale.value
+    )
+  );
+});
+</script>
