1e130c9b0bbae43c7f3d35d37c74e0bf6eee5837
Levente26 <madi.levente26@gmail.com>
2023-05-04 12:20:29 +0200

    feat: add strapi connector plugin to nuxt

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    <NuxtWelcome />
+    {{ data }}
   </div>
 </template>
+
+<script setup>
+const { find } = useStrapi()
+
+const { data, pending, refresh, error } = await useAsyncData(
+  "warehouse",
+  () => find('warehouses')
+);
+
+console.log(data);
+</script>
